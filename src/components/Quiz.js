31f8ad8c10b93767387f3856a1639c8ea0b059e8
Juris Guevara <tertullanojuris@gmail.com>
2022-12-03 00:04:51 +0800

    version 0.5  - updated score script  - improved quiz list UI  - completed development

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import useFetch from "../customHooks/useFetch";
 
 const Quiz = ({title, categoryPath, modePath}) => {
@@ -16,60 +16,90 @@ const Quiz = ({title, categoryPath, modePath}) => {
   }
 
   const {data, isPending, error} = useFetch(`https://the-trivia-api.com/api/questions?categories=${categoryPath}&limit=${setMode()}&difficulty=${modePath}`)
+  const [quizEnd, setQuizEnd] = useState(false)
+  const [displayScore, setDisplayScore] = useState(0)
   let correctAnswers = []
+  let shuffledKeys = []
+  let score = 0
 
   const setAnswer = (e) => {
     e.target.firstElementChild.checked = true
-    console.log(e.target.firstElementChild.value)
   }
 
   const shuffleAnswers = (correct, aIncorrect, id) => {
-    let answers = [correct]
+    let answers = [{'correct': correct}]
     correctAnswers.push(correct)
-    aIncorrect.map(inc => answers.push(inc))
+    aIncorrect.map(inc => answers.push({'inc': inc}))
 
     let shuffled = answers
     .map(value => ({ value, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
     .map(({value}) => value)
 
+    let shuffledValues = []
+
+    shuffled.forEach(function(obj){
+    for (var key in obj){
+        shuffledValues.push(obj[key])
+        shuffledKeys.push(key)
+      }
+    });
+
+    shuffledKeys.push('inc')
+
     return <Fragment>
-      {shuffled.map(sItem =>
+      {shuffledValues.map(sItem =>
         <li onClick={(e) => setAnswer(e)} key={sItem}>
           <input className="answer" onClick={(e) => e.stopPropagation} type="radio" name={id} value={sItem} />
           {sItem}
         </li>)}
         <li onClick={(e) => setAnswer(e)}>
-        <input className="answer" onClick={(e) => e.stopPropagation} type="radio" name={id} value="none" defaultChecked />
+          <input className="answer" onClick={(e) => e.stopPropagation} type="radio" name={id} value="none" defaultChecked />
           I don't know
         </li>
     </Fragment>
   }
 
   const submitQuiz = () => {
-    let x = document.querySelectorAll('.answer')
-    let y = []
+    let renderedAnswers = document.querySelectorAll('.answer')
+    let selectedAnswers = []
 
-    x.forEach(element => {
+    renderedAnswers.forEach((element, i) => {
+      element.parentNode.classList.add('disable-radio')
+
       if(element.checked) {
-        y.push(element)
+        selectedAnswers.push(element)
+      }
+
+      if(shuffledKeys[i] === 'correct') {
+        element.parentNode.classList.add('correct')
       }
     });
 
-    y.forEach((element, i) => {
+
+
+    selectedAnswers.forEach((element, i) => {
       if(element.value === correctAnswers[i]) {
         element.parentNode.classList.add('yes-point')
-        console.log(element, "tama")
+        score += 1
       } else {
         element.parentNode.classList.add('no-point')
-        console.log(element, "mali")
       }
     })
+
+    setDisplayScore(score)
+
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+    
+    setTimeout(() => {
+      setQuizEnd(true)  
+    }, 100);
   }
 
   return (
     <div className="container">
       <h1>{title}</h1>
+      {quizEnd && <h2>Score: {displayScore}/{setMode()}</h2>}
       {isPending && <div>Loading . . .</div>}
       {error && <div>{error}</div>}
       {data &&
@@ -84,10 +114,10 @@ const Quiz = ({title, categoryPath, modePath}) => {
             </li>
           )}
         </ol>
-        <div className="submit">
+        {!quizEnd && <div className="submit">
           <h2>Submit Quiz?</h2>
           <button onClick={submitQuiz}>Yes</button>
-        </div>
+        </div>}
       </>}
     </div>
   );
